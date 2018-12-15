715633a7b7ce922536e115de6cdde7a763cc5487
Anilop <polinadh@gmail.com>
2018-12-15 19:47:41 +0100

    Update mnist_keras_cnn.py

--- CNN with Keras/mnist_keras_cnn.py
+++ CNN with Keras/mnist_keras_cnn.py
@@ -4,30 +4,19 @@ from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
 
-#download mnist data and split into train and test sets
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 
-#plot the first image in the dataset
-plt.imshow(X_train[0])
-
-
-#check image shape
-X_train[0].shape
-
-#reshape data to fit model
 X_train = X_train.reshape(60000,28,28,1)
 X_test = X_test.reshape(10000,28,28,1)
 
 
-
-#one-hot encode target column
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 y_train[0]
 
 
-#create model
 model = Sequential()
 #add model layers
 model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
@@ -36,22 +25,8 @@ model.add(Flatten())
 model.add(Dense(10, activation='softmax'))
 
 
-#compile model using accuracy to measure model performance
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-
-#train the model
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3)
 
-
-#predict first 4 images in the test set
-model.predict(X_test[:4])
-
-
-#actual results for first 4 images in test set
-y_test[:4]
-
-
-
-
-
+model.predict(X_test[:10])
